fa93462bd7f8785bc5c84d5cfb19c851eb5af1eb
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-09-24 22:41:27 +0530

    feat: add dependencies and keymap options for completion configuration in completion.lua

--- .config/nvim/lua/mine/plugins/coding/completion.lua
+++ .config/nvim/lua/mine/plugins/coding/completion.lua
@@ -1,6 +1,9 @@
 return {
   {
     "saghen/blink.cmp",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+    },
     opts = {
       completion = {
         documentation = {
@@ -9,6 +12,22 @@ return {
       },
       signature = {
         enabled = true,
+      },
+      snippets = {
+        preset = "default",
+        search_paths = { vim.fn.stdpath("config") .. "/snippets" },
+      },
+      keymap = {
+        preset = "default",
+
+        -- Navigate down the menu, or jump to next snippet placeholder
+        ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+
+        -- Navigate up the menu, or jump to previous snippet placeholder
+        ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+
+        -- Accept the highlighted completion item / expand snippet
+        ["<CR>"] = { "accept", "fallback" },
       },
     },
   },
